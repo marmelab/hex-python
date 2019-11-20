728c423e0f1f7c91734e92ad59ea8918ab5d8071
Christophe Lecroq <christophe@marmelab.com>
2019-11-20 18:15:09 +0100

    Fix the bug where a stone is put on external border of the board

--- src/board/board.py
+++ src/board/board.py
@@ -6,8 +6,8 @@ def put_stone(coords, board):
     if coords is None:
         raise ValueError("Coordinates for the stone is incorrect. Please, pick a new one")
 
-    line = get_x_index(coords[0])
-    column = int(coords[1])
+    line = get_x_index(coords[0]) - 1
+    column = int(coords[1]) - 1
 
     if is_outside(board, line, column):
         raise ValueError("Stone can't be put outside the board")
@@ -15,7 +15,7 @@ def put_stone(coords, board):
     if is_already_taken(board, line, column):
         raise ValueError("A stone already exists at this position")
 
-    new_board[column - 1][line - 1] = 1
+    new_board[column][line] = 1
 
     return new_board
 
